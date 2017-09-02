afbdc139388f58643327dcc406ab7535f75825fe
Luiz Eduardo Sol <luizedusol@gmail.com>
2017-09-02 19:37:04 -0300

    Implementação básica da classe ImageProcessor

--- EP1b/src/image_processor.py
+++ EP1b/src/image_processor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 import copy
@@ -7,46 +7,232 @@ import cv2
 import numpy as np
 
 class ImageProcessor(object):
-    """docstring for ImageProcessor"""
+    """A class responsible for storing and processing images.
+
+    Attributes:
+        image: A virtual attribute to implement the setter and getter of _image
+        _image: The real attribute responsible for storing the class image
+    """
+
     def __init__(self, image=None):
-        super(ImageProcessor, self).__init__()
+        """Inits ImageProcessor with an optional image."""
         if image:
             self.image = image
+        else:
+            self._image = None
 
     @property
     def image(self):
+        """The image getter.
+
+        The true image object is incapsulated on the _image attribute,
+        therefore the user must use the image virtual attribute to interact
+        with image stored in this class
+
+        Returns:
+            A numpy.ndarray containing the image.
+        """
         return self._image
 
     @image.setter
-    def image(self, other):
-        if isinstance(other, np.ndarray):
-            self._image = other
-        elif isinstance(other, str):
-            self._image = cv2.imread(other, cv2.IMREAD_UNCHANGED)
+    def image(self, value):
+        """The image setter.
+
+        Args:
+            value: either a numpy.ndarray containing a image or the path to an
+                image.
+
+        Raises:
+            TypeError: The value is neither an image nor the path to an image.
+        """
+        if isinstance(value, np.ndarray):
+            self._image = value
+        elif isinstance(value, str):
+            self._image = cv2.imread(value, cv2.IMREAD_UNCHANGED)
         else:
             raise TypeError
 
-    def show_image(self, window_name=None):
-        window_name = 'image' if not window_name else window_name
-        cv2.imshow(window_name, self.image)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+    def has_image(self):
+        """Determines whether an image is already stored.
 
-    def save_image(self, path=None):
-        path = 'image_processor_output.bmp' if not path else path
+        Returns:
+            True if there's already an image stored, False otherwise.
+        """
+        if self.image is None:
+            return False
+        return True
+
+    def show_image(self, window_name='image'):
+        """Displays the current image.
+
+        Keyword Args:
+            window_name (='image'): the name of the window in which the image
+                will be displayed
+        """
+        cv2.imshow(window_name, self.image)
+
+    def save_image(self, path='image_processor_output.bmp'):
+        """Saves the current image on the file system.
+
+        Keyword Args:
+            path (='image_processor_output'): the path where the file should be
+                saved
+        """
         cv2.imwrite(path, self.image)
 
-    def get_laplacian(self):
-        return cv2.Laplacian(self.image, cv2.CV_64F)
+    def get_laplacian(self, data_type=cv2.CV_32F):
+        """Calculates the Laplacian of the image.
 
-    def apply_laplacian(self):
-        self.image = self.get_laplacian()
+        Keyword Args:
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
 
-    def x_grad(self, kernel_size=5):
-        return cv2.Sobel(self.image, cv2.CV_64F, 1, 0, kernel_size)
+        Returns:
+            The resulting Laplacian matrix.
+        """
+        return cv2.Laplacian(self.image, data_type)
 
-    def y_grad(self, kernel_size=5):
-        return cv2.Sobel(self.image, cv2.CV_64F, 0, 2, kernel_size)
+    def apply_laplacian(self, data_type=cv2.CV_32F):
+        """Substitutes the current image for it's Laplacian.
+
+        Keyword Args:
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        self.image = self.get_laplacian(data_type=data_type)
+
+    def show_laplacian(self, window_name='image', data_type=cv2.CV_32F):
+        """Shows the Laplacian of the image.
+
+        Keyword Args:
+            window_name (='image'): the name of the window in which the image
+                will be displayed
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        cv2.imshow(window_name, self.get_laplacian(data_type=data_type))
+
+    def get_x_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Calculates the Sobel gradient of the image on the X axis.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+
+        Returns:
+            The resulting unidirectional Gradient matrix.
+        """
+        return cv2.Sobel(self.image, data_type, 1, 0, kernel_size)
+
+    def apply_x_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Substitutes the current image for it's Sobel's X axis gradient.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        self.image = self.get_x_grad(kernel_size=kernel_size,
+                                     data_type=data_type)
+
+    def show_x_grad(self, window_name='image', kernel_size=5,
+                    data_type=cv2.CV_32F):
+        """Shows the Sobel's X axis gradient of the image.
+
+        Keyword Args:
+            window_name (='image'): the name of the window in which the image
+                will be displayed
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        cv2.imshow(window_name, self.get_x_grad(kernel_size=kernel_size,
+                                                data_type=data_type))
+
+    def get_y_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Calculates the Sobel gradient of the image on the Y axis.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+
+        Returns:
+            The resulting unidirectional Gradient matrix.
+        """
+        return cv2.Sobel(self.image, data_type, 0, 2, kernel_size)
+
+    def apply_y_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Substitutes the current image for it's Sobel's Y axis gradient.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        self.image = self.get_y_grad(kernel_size=kernel_size,
+                                     data_type=data_type)
+
+    def show_y_grad(self, window_name='image', kernel_size=5,
+                    data_type=cv2.CV_32F):
+        """Shows the Sobel's Y axis gradient of the image.
+
+        Keyword Args:
+            window_name (='image'): the name of the window in which the image
+                will be displayed
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        cv2.imshow(window_name, self.get_y_grad(kernel_size=kernel_size,
+                                                data_type=data_type))
+
+    def get_grayscale(self):
+        """Gets the Grayscale version of the image.
+
+        Returns:
+            The Grayscale version of the image.
+        """
+        return cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+    def apply_grayscale(self):
+        """Substitutes the current image for it's Grayscale version."""
+        self.image = self.get_grayscale()
+
+    def show_grayscale(self, window_name='image'):
+        """Shows the current image's Grayscale version."""
+        cv2.imshow(window_name, self.get_grayscale())
+
+    def get_complex_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Calculates the current image's complex gradient matrix.
+
+        This method assing the X axis gradient to the real part and Y axis
+        gradient to the complex part of a matrix.
+
+        See get_y_grad() and get_x_grad() for further documentation.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+
+        Returns:
+            The resulting complex Gradient matrix.
+        """
+        grad_x = self.get_x_grad(kernel_size=kernel_size, data_type=data_type)
+        grad_y = self.get_x_grad(kernel_size=kernel_size, data_type=data_type)
+
+        return grad_x + (0 + 1j) * grad_y
+
+    def apply_complex_grad(self, kernel_size=5, data_type=cv2.CV_32F):
+        """Substitutes the current image for it's complex gradient.
+
+        Keyword Args:
+            kernel_size (=5): the kernel size of the Sobel filter
+            data_type (=cv2.CV_32F): the datatype to be used on the calculation
+        """
+        self.image = self.get_complex_grad(kernel_size=kernel_size,
+                                           data_type=data_type)
+
+    def lines(self):
+        """Determines the number of lines of the image."""
+        return self.image.shape[0]
+
+    def cols(self):
+        """Determines the number of columns of the image."""
+        return self.image.shape[1]
 
     def copy(self):
+        """Genetates a deep copy of this class instance."""
         return copy.deepcopy(self)
--- EP1b/src/main.py
+++ EP1b/src/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 import cv2
@@ -7,7 +7,17 @@ import numpy as np
 from image_processor import *
 
 if __name__ == '__main__':
-    img1 = ImageProcessor('img/img0105.bmp')
-    img.show_image(window_name="Imagem")
-    img2 = img1.copy()
-    img2.laplacian()
+    img = ImageProcessor('img/img0105.bmp')
+    img.show_image(window_name="Original Image")
+    img.show_laplacian(window_name="Laplacian")
+    img.show_x_grad(window_name="X-Gradient")
+    img.show_y_grad(window_name="Y-Gradient")
+
+    img2 = img.copy()
+    img2.apply_laplacian()
+    img2.show_grayscale(window_name='Grayscale grad')
+
+    print img.get_complex_grad()
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
